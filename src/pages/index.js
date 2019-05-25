afd94569e22a9d6f349210d37c33bf86504ff643
Pav Sidhu <pav@pavsidhu.com>
2019-05-25 18:08:43 +0100

    Clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hey I'm Pav</h1>
+    <ul>
+      <li>Develoepr</li>
+      <li>UX Designer</li>
+      <li>Powerlifter</li>
+      <li>Cardiff / Birmingham</li>
+      <li>Computer Science</li>
+    </ul>
   </Layout>
 )
 
