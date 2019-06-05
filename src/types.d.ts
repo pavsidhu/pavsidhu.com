be301c5425aa87951a06e730747bc7e32db3c1a6
Pav Sidhu <pav@pavsidhu.com>
2019-06-05 01:15:41 +0100

    Clean up project list

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,15 @@
 declare module "*.png" {}
 declare module "*.svg" {}
 declare module "*.otf" {}
+
+interface IProject {
+  id: number
+  name: string
+  description: string
+  fullDescription: string[]
+  link: string
+  theme: {
+    background: string
+  }
+  render: (project: IProject) => JSX.Element
+}
