60ad88ec69d4236d5ae70d9c578af9587e2254f3
Pav Sidhu <pav@pavsidhu.com>
2019-06-29 14:57:36 +0100

    Fix case 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Intro from "../components/Intro"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
 import Social from "../components/Social"
