ca969f8d3fdc18c60755e7ff804cfc5290fe2036
Pav Sidhu <pav@pavsidhu.com>
2019-06-29 22:41:30 +0100

    Forgot to preload spotiparty and whatstrending

--- src/hooks/usePreloadImages.ts
+++ src/hooks/usePreloadImages.ts
@@ -1,13 +1,5 @@
 import { useEffect, useState } from "react"
 
-import moon from "../images/bulb/moon.svg"
-import bulbPreview from "../images/bulb/preview.svg"
-import bulbStars from "../images/bulb/stars.svg"
-import copbotPreview from "../images/copbot/preview.png"
-import daynotePreview from "../images/daynote/preview.png"
-import laptop from "../images/revisify/laptop.svg"
-import notepad from "../images/revisify/notebook.svg"
-import revisifyPreview from "../images/revisify/preview.png"
 import asteroid from "../images/rocket-riot/asteroid.svg"
 import planet1 from "../images/rocket-riot/planet-1.svg"
 import planet2 from "../images/rocket-riot/planet-2.svg"
@@ -16,6 +8,22 @@ import rocket1 from "../images/rocket-riot/rocket-1.svg"
 import rocket2 from "../images/rocket-riot/rocket-2.svg"
 import rocket3 from "../images/rocket-riot/rocket-3.svg"
 import rocketRiotStars from "../images/rocket-riot/stars.svg"
+
+import laptop from "../images/revisify/laptop.svg"
+import notepad from "../images/revisify/notebook.svg"
+import revisifyPreview from "../images/revisify/preview.png"
+
+import copbotPreview from "../images/copbot/preview.png"
+
+import spotipartyPreview from "../images/spotiparty/preview.png"
+
+import moon from "../images/bulb/moon.svg"
+import bulbPreview from "../images/bulb/preview.svg"
+import bulbStars from "../images/bulb/stars.svg"
+
+import daynotePreview from "../images/daynote/preview.png"
+
+import echoDot from "../images/whats-trending/echo-dot.png"
 
 function usePreloadImage(src: string) {
   const [isPreloaded, setPreloaded] = useState(false)
@@ -37,14 +45,6 @@ function usePreloadImages(images: string[]) {
 
 export default function usePreloadAllImages() {
   usePreloadImages([
-    moon,
-    bulbPreview,
-    bulbStars,
-    copbotPreview,
-    daynotePreview,
-    laptop,
-    notepad,
-    revisifyPreview,
     asteroid,
     planet1,
     planet2,
@@ -52,6 +52,22 @@ export default function usePreloadAllImages() {
     rocket1,
     rocket2,
     rocket3,
-    rocketRiotStars
+    rocketRiotStars,
+
+    laptop,
+    notepad,
+    revisifyPreview,
+
+    copbotPreview,
+
+    spotipartyPreview,
+
+    moon,
+    bulbPreview,
+    bulbStars,
+
+    daynotePreview,
+
+    echoDot
   ])
 }
