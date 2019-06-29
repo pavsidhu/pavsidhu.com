a09fc0fbae27fb47096f8c139ff4a5f1d325d6e7
Pav Sidhu <pav@pavsidhu.com>
2019-06-29 12:21:39 +0100

    Add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,47 @@
 import React from "react"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
+import Link from "../components/Link"
 import Seo from "../components/Seo"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: #fefefe;
+  padding: 24px;
+`
+
+const Emoji = styled.p`
+  font-size: 8rem;
+  line-height: 8rem;
+  margin-bottom: 0;
+`
+
+const Title = styled.h1`
+  font-size: 4.8rem;
+  color: #1b1b1b;
+  margin-bottom: 16px;
+`
+
+const Text = styled.p`
+  font-size: 1.8rem;
+  color: #1b1b1b;
+  text-align: center;
+`
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Seo title="404 Are you lost?" />
+      <Emoji>️☹️</Emoji>️<Title>Are you lost?</Title>
+      <Text>
+        There's nothing here, <Link href="/">click here</Link> to go back home.
+      </Text>
+    </Container>
   </Layout>
 )
 
