626b99c73a8581657815eb891166c8c0f5528195
Pav Sidhu <pav@pavsidhu.com>
2019-10-13 16:06:40 +0100

    Make Feeling button more visible

--- src/components/Projects/Feeling.tsx
+++ src/components/Projects/Feeling.tsx
@@ -42,7 +42,7 @@ const Button = styled.a`
   padding: 16px;
   border-radius: 12px;
   background-color: rgba(254, 254, 254, 0.8);
-  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.15);
   border-radius: 4px;
   transition: all 80ms;
 
