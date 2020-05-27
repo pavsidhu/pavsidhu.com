8d90f1f53ef9d8aca1307db7d0da9f3e0c77ee93
Pav Sidhu <pav@pavsidhu.com>
2020-05-27 13:43:34 +0100

    Remove some old files from the previous version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
 import React from "react"
 
-import Intro from "../components/Intro"
-import Layout from "../components/Layout"
-import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-import Social from "../components/Social"
 
 export default function IndexPage() {
   return (
-    <Layout>
+    <>
       <Seo title="Home" />
-
-      <Intro />
-      <Projects />
-      <Social />
-    </Layout>
+    </>
   )
 }
