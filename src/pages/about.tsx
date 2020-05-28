ba67219144cacc8ef53d440d943175c97d2a04ec
Pav Sidhu <pav@pavsidhu.com>
2020-05-28 11:04:25 +0100

    Add about, projects and contact pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+import Seo from "../components/Seo"
+
+export default function AboutPage() {
+  return (
+    <>
+      <Seo title="About" />
+    </>
+  )
+}
