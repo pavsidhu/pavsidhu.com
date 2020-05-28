d5a1c4897f40c9e936b2f328a4726df1c07207f6
Pav Sidhu <pav@pavsidhu.com>
2020-05-28 10:48:40 +0100

    Convert SVGs to be inline

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,14 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /images\/icons/
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Pav Sidhu`,
