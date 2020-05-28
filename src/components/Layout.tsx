938df40b9734632f053dce7ad141d0ca786d2edd
Pav Sidhu <pav@pavsidhu.com>
2020-05-28 21:57:51 +0100

    Disable animations if prefers reduce motion

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -36,6 +36,20 @@ const GlobalStyle = createGlobalStyle`
     font-style: normal;
   }
 
+  @media (prefers-reduced-motion: reduce) {
+    *,
+    ::before,
+    ::after {
+      animation-delay: -1ms !important;
+      animation-duration: 1ms !important;
+      animation-iteration-count: 1 !important;
+      background-attachment: initial !important;
+      scroll-behavior: auto !important;
+      transition-delay: 0s !important;
+      transition-duration: 0s !important; 
+    } 
+  }
+
   * {
     margin: 0;
     padding: 0;
