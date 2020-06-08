c6245f28ff755cb39ca5e2be2c88b8a5997d06c2
Pav Sidhu <pav@pavsidhu.com>
2020-06-08 15:52:31 +0100

    Add YouTube MDX component

---
+++ src/components/YouTube.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%;
+`
+
+const Iframe = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+`
+
+export default function YouTube({ link }: { link: string }) {
+  const url = new URL(link)
+  const searchParams = new URLSearchParams(url.search)
+  const id = searchParams.get("v")
+
+  return (
+    <Container>
+      <Iframe
+        src={`https://www.youtube-nocookie.com/embed/${id}`}
+        allowFullScreen={true}
+      />
+    </Container>
+  )
+}
