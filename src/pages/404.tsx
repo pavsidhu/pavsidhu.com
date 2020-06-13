eae49879771b1d260e9bce43f2fb03b115cf23e5
Pav Sidhu <pav@pavsidhu.com>
2020-06-14 00:25:26 +0100

    Add simple 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,42 +1,41 @@
 import React from "react"
 import { styled } from "linaria/react"
+import { Link } from "gatsby"
 
 import { Seo } from "../components"
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  background-color: #fefefe;
-  padding: 24px;
-`
-
-const Emoji = styled.p`
-  font-size: 8rem;
-  line-height: 8rem;
-  margin-bottom: 0;
+const Container = styled.article`
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  gap: var(--space-s);
+  padding: var(--space-m);
 `
 
 const Title = styled.h1`
-  font-size: 4.8rem;
-  color: #1b1b1b;
-  margin-bottom: 16px;
+  font-size: var(--font-xxl);
 `
 
 const Text = styled.p`
   font-size: var(--font-m);
-  color: #1b1b1b;
-  text-align: center;
+  line-height: 3.2rem;
+`
+
+const HomeLink = styled((props) => <Link {...props} />)`
+  font-size: var(--font-m);
+  color: var(--primary-color);
 `
 
 export default function NotFoundPage() {
   return (
     <Container>
-      <Seo title="404 Are you lost?" />
-      <Emoji>️☹️</Emoji>️<Title>Are you lost?</Title>
-      <Text>There's nothing here, go back home.</Text>
+      <Seo title="404" />
+      <Title>Page Not Found</Title>
+      <Text>
+        There's nothing here, perhaps you'll find what you're looking for back
+        home
+      </Text>
+      <HomeLink to="/">Go To Home</HomeLink>.
     </Container>
   )
 }
