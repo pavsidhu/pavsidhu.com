bb824eee1e98b1098f3dafd24f7df91ed167fd4b
Pav Sidhu <pav@pavsidhu.com>
2020-06-18 09:40:04 +0100

    Fix build step (again) 😠

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,7 +40,7 @@ exports.createPages = async ({ graphql, actions }) => {
 }
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "develop-html") {
+  if (stage === "develop-html" || stage === "build-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
