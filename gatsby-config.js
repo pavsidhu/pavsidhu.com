ef84c561015ea7fa7a1faa988da49e9243a4ebaa
Pav Sidhu <pav@pavsidhu.com>
2020-06-18 11:32:46 +0100

    Use automatic icons and disable favicons in gatsby manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,56 +91,11 @@ module.exports = {
         theme_color: `#fefefe`,
         display: `standalone`,
         cache_busting_mode: "none",
-        icons: [
-          {
-            src: "icons/48.png",
-            sizes: "48x48",
-            type: "image/png",
-            purpose: "badge any"
-          },
-          {
-            src: "icons/72.png",
-            sizes: "72x72",
-            type: "image/png",
-            purpose: "badge any"
-          },
-          {
-            src: "icons/96.png",
-            sizes: "96x96",
-            type: "image/png",
-            purpose: "badge any"
-          },
-          {
-            src: "icons/144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "maskable any"
-          },
-          {
-            src: "icons/192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable any"
-          },
-          {
-            src: "icons/256.png",
-            sizes: "256x256",
-            type: "image/png",
-            purpose: "maskable any"
-          },
-          {
-            src: "icons/384.png",
-            sizes: "384x384",
-            type: "image/png",
-            purpose: "maskable any"
-          },
-          {
-            src: "icons/512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable any"
-          }
-        ]
+        icon: "src/images/icon.png",
+        icon_options: {
+          purpose: "maskable any"
+        },
+        include_favicon: false
       }
     },
     {
