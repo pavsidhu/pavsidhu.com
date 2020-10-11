3abb378f3ee5047d0a3e1b982ad0077e788000d4
Pav Sidhu <pav@pavsidhu.com>
2020-10-11 15:44:57 +0100

    Force reload new changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,3 @@
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Would you like to reload to display the latest version?`
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
+  window.location.reload()
 }
