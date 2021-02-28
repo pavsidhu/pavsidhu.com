d6c14bec72ab8230a2daca0cc3b7483df276dbfe
Pav Sidhu <pav@pavsidhu.com>
2021-02-28 23:10:29 +0000

    Add links to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Image from "gatsby-image"
 import { styled } from "linaria/react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import { Seo } from "../components"
 
@@ -58,24 +58,24 @@ export default function AboutPage({ data }) {
       <Paragraph>
         A few years later, I began to explore other areas of programming. Whilst
         studying for my school exams, I used my skills to build a study website
-        called Revisify. Over a 2 year period, Revisify grew into a community of
-        3000 students. I decided to shut the website down in 2017 so I could
-        pursue other interests.
+        called <Link to="/projects#revisify">Revisify</Link>. Over a 2 year
+        period, Revisify grew into a community of 3000 students. I decided to
+        shut the website down in 2017 so I could pursue other interests.
       </Paragraph>
       <Paragraph>
         After finishing school, I went on to achieve a first in Artificial
         Intelligence and Computer Science at the University of Birmingham. I
         learnt a variety of topics including machine learning, neural networks
         and natural language processing. As part of my final year project, I
-        built a dating chatbot that matched users based on their personality. My
-        neural network managed to achieve excellent results in personality
-        classification from text.
+        built <Link to="/projects#aida">Aida</Link>, a dating chatbot that
+        matched users based on their personality. My neural network managed to
+        achieve excellent results in personality classification from text.
       </Paragraph>
       <Paragraph>
         I’ve worked on several other projects through hackathons, self-interests
         and university. If you’re reading this, I’m most likely working on
         something new. I'm currently interested in progressive web apps and the
-        future of the web.
+        future of the open web.
       </Paragraph>
       <Paragraph>
         When I'm not behind my computer, you'll find me lifting weights in the
