b219edbe501b20d4c8393b1999c255335726ec08
Pav Sidhu <pav@pavsidhu.com>
2026-07-20 23:48:41 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,7 +17,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1"
       },
       {
-        title: "yo"
+        title: "yoo"
       },
       {
         name: "description",
